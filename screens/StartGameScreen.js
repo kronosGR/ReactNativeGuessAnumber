7a07dacbdd2f3a1b782e01600508b99d1e07a391
KronosGR <geo.elgeo@gmail.com>
2022-02-05 08:07:22 +0100

    color theme with constants

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 import Card from '../components/Card';
 
+import Colors from '../constants/colors'
+
 const StartGameScreen = () => {
   return (
     <View style={styles.screen}>
@@ -10,8 +12,8 @@ const StartGameScreen = () => {
         <Text>Select a Number</Text>
         <TextInput />
         <View style={styles.buttonContainer}>
-          <Button title='Reset' onPress={() => {}} />
-          <Button title='Confirm' onPress={() => {}} />
+          <View style={styles.button}><Button title='Reset' onPress={() => {}}  color={Colors.accent}/></View>
+          <View style={styles.button}><Button title='Confirm' onPress={() => {}} color={Colors.primary}/></View>
         </View>
       </Card>
     </View>
@@ -39,6 +41,10 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
     paddingHorizontal: 15,
   },
+  button:{
+    width: 100,
+    marginHorizontal:10
+  }
 });
 
 export default StartGameScreen;
